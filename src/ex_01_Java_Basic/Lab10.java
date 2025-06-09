b5a052b446bc2b1a736ee64d7cb16671f0074220
Deepika <support@ods.com>
2025-06-09 18:31:34 +0530

    9th June 2025

---
+++ src/ex_01_Java_Basic/Lab10.java
@@ -0,0 +1,13 @@
+package ex_01_Java_Basic;
+
+public class Lab10 {
+    public static void main(String[] args) {
+        System.out.println("This will not work.");
+        // '_' is a keyword, and may not be used as an identifier
+        /*
+        Starting from Java 1.2 (JDK 1.2) and onwards, you cannot execute a Java program with just _ as the class name. It will result in a compilation error.
+        Yes, there was a time in early versions (specifically Java 1.1) when it was allowed, but this was fixed in later versions.
+         */
+    }
+}
+
