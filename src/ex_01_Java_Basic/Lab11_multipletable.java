61809ff6e13146f3cf949d68dca7fd461176da67
Deepika <support@ods.com>
2025-06-11 07:27:45 +0530

    new10

---
+++ src/ex_01_Java_Basic/Lab11_multipletable.java
@@ -0,0 +1,18 @@
+package ex_01_Java_Basic;
+
+public class Lab11_multipletable {
+    public static void main(String[] args) {
+        int number = 9;
+        System.out.printf("%d x 1 = %d%n", number, number * 1);
+        System.out.printf("%d x 2 = %d%n", number, number * 2);
+        System.out.printf("%d x 3 = %d%n", number, number * 3);
+        System.out.printf("%d x 4 = %d%n", number, number * 4);
+        System.out.printf("%d x 5 = %d%n", number, number * 5);
+        System.out.printf("%d x 6 = %d%n", number, number * 6);
+        System.out.printf("%d x 7 = %d%n", number, number * 7);
+        System.out.printf("%d x 8 = %d%n", number, number * 8);
+        System.out.printf("%d x 9 = %d%n", number, number * 9);
+        System.out.printf("%d x 10 = %d%n", number, number * 10);
+    }
+}
+
